4e1f8bcee44d1c9270973a79a3329ffefcaeb935
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-03 16:17:02 +0000

    Formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    restoreMocks: true,
-  },
+	test: {
+		restoreMocks: true
+	}
 });
