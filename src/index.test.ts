8d6535db37d0d7fd3f33c9ff7883af054086152b
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-05 06:56:16 +0000

    Check that flatMap generates a new tree with deep structural equality

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,7 @@ import { u } from 'unist-builder';
 import { describe, expect, it, vi } from 'vitest';
 import flatMap from './index.js';
 
-describe('transform', () => {
+describe('flatMap', () => {
 	const tree = u('tree', [
 		u('leaf', 'leaf 1'),
 		u('node', [u('leaf', 'leaf 2')]),
@@ -10,19 +10,18 @@ describe('transform', () => {
 		u('leaf', 'leaf 3')
 	]);
 
-	const flatMapFunction = (node: import('unist').Node) =>
-		node.type === 'leaf' ? [u('leaf', 'Modified')] : [node];
+	const fn = vi.fn((node: import('unist').Node) => [
+		node.type === 'leaf' ? u('leaf', 'Modified') : node
+	]);
 
 	it('should apply the callback function to each node in the tree', () => {
-		const flatMapFunction = vi.fn((node: import('unist').Node) => [node]);
+		void flatMap(tree, fn);
 
-		void flatMap(tree, flatMapFunction);
-
-		expect(flatMapFunction).toHaveBeenCalledTimes(6);
+		expect(fn).toHaveBeenCalledTimes(6);
 	});
 
 	it('should return the modified tree', () => {
-		const result = flatMap(tree, flatMapFunction);
+		const result = flatMap(tree, fn);
 
 		expect(result).toEqual(
 			u('tree', [
@@ -35,7 +34,7 @@ describe('transform', () => {
 	});
 
 	it('should not mutate the source tree', () => {
-		void flatMap(tree, flatMapFunction);
+		void flatMap(tree, fn);
 
 		expect(tree).toEqual(
 			u('tree', [
@@ -45,5 +44,28 @@ describe('transform', () => {
 				u('leaf', 'leaf 3')
 			])
 		);
+	});
+
+	it('should return a new tree deeply structually equal to the original', () => {
+		const isParent = (node: import('unist').Node): node is import('unist').Parent =>
+			'children' in node && Array.isArray(node.children);
+
+		const deepStructualEquality = (a: import('unist').Node, b: import('unist').Node) => {
+			expect(a).not.toBe(b);
+			expect(a).toEqual(b);
+
+			if (isParent(a) && isParent(b)) {
+				expect(a.children).not.toBe(b.children);
+				expect(a.children).toEqual(b.children);
+
+				a.children.forEach((child, i) => deepStructualEquality(child, b.children[i]));
+			} else if (isParent(a) || isParent(b)) {
+				throw new Error('Nodes are not of the same type');
+			}
+		};
+
+		const result = flatMap(tree, (node) => [node]);
+
+		expect(() => deepStructualEquality(result, tree)).not.toThrow();
 	});
 });
